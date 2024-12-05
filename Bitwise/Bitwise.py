f379d8ad03f23dc4c5eeba4fed3ffc611c1ac821
Phong Le <le.duc.phong@gmail.com>
2024-12-04 19:10:13 -0500

    modified:   Bitwise/Bitwise.py

--- Bitwise/Bitwise.py
+++ Bitwise/Bitwise.py
@@ -1,6 +1,3 @@
-# Get a bit of n at a specific position 
-def get_bit(num, pos):
-    return (num & (1 << pos)) >> pos
 
 # Unset/Clear a bit, i.e., bit 1-->0
 def unset_bit(num, pos):
@@ -10,6 +7,37 @@ def unset_bit(num, pos):
 def toggle_bit(num, pos):
     return num ^ ((1 << pos))
 
+def set_bit(num, pos):
+    bit = num & (1 << pos) >> pos
+    if bit != 0:    # toggle if the bit is 0
+        return num ^ (1 << pos)
+    
+    return num 
+
+# Get a bit of n at a specific position 
+def get_bit(num, pos):
+    return (num & (1 << pos)) >> pos
+
+# Get n bits from pos to the left
+def get_n_bits(num, pos, n):
+    s = '1'* n
+    k = int(s, 2)
+    return (num & (k << pos)) >> pos
+
+# Relace n bits of num from position pos by a value of n bits
+def replace_n_bits(num, value, pos, n):
+    s_num = format(num, 'b')
+    s_value = format(value, 'b')
+
+    if len(s_value) > n:
+        print("Number of bits of replaced value is bigger than", n)
+        return 
+    
+    if len(s_value) < n:
+        s_value = '0'*(n - len(s_value)) + s_value
+
+    for _ in range(n):
+         
 
 # Check if a number is a power of two 
 def is_power_two(num):
