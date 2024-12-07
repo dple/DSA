d92fdc5e7ed96a8179c8410afb6df6a8b4e6a36f
Phong Le <le.duc.phong@gmail.com>
2024-12-06 20:20:50 -0500

    new file:   List/minSwaps.py new file:   List/sortArray.py

---
+++ List/minSwaps.py
@@ -0,0 +1,47 @@
+"""
+https://www.geeksforgeeks.org/minimum-number-swaps-required-sort-array/
+
+Given an array of N distinct elements, find the minimum number of swaps required to sort the array.
+
+Examples: 
+
+Input: {4, 3, 2, 1}
+Output: 2
+Explanation: Swap index 0 with 3 and 1 with 2 to form the sorted array {1, 2, 3, 4}
+
+
+
+Input: {1, 5, 4, 3, 2}
+Output: 2
+"""
+
+def min_swaps(L):
+    states = [0] * len(L)    # array record visited elements
+    no_swaps = 0
+    #i = 0
+
+    for i in range(len(L)):
+        # If element e at position i is correct (i.e., e == i + 1) or if the position was visited, then skip
+        if (L[i] == i + 1) or states[i]:   
+            continue #state[i] ^= 1     i += 1
+
+        else:            
+            # Find a cycle            
+            states[i] = 1
+            cycle_size = 2          # Cycle is at least 2
+            j = L[i] - 1            # get the correct position of L[i]
+            states[j] = 1
+            while L[j] != i + 1:
+                j = L[j] - 1
+                states[j] = 1
+                cycle_size += 1
+
+            no_swaps += cycle_size - 1
+    
+    return no_swaps
+        
+                
+
+if __name__ == '__main__':
+    L = [1, 5, 4, 3, 2]
+    print("Minimum number of swaps is: ", min_swaps(L))
---
+++ List/sortArray.py
@@ -0,0 +1,37 @@
+"""
+We are given an array that contains 1 to n elements, our task is to sort this array in an efficient way. We are not allowed to simply copy the numbers from 1 to n.
+Examples : 
+
+Input : arr[] = {2, 1, 3};
+Output : {1, 2, 3}
+
+
+Input : arr[] = {2, 1, 4, 3};
+Output : {1, 2, 3, 4} 
+"""
+
+# Using sort algorithm. Complexity: O(n log n)
+def sort_sorting(L):
+    L.sort()
+    return L
+
+# Complexity: O(n)
+def linear_sort(L):
+    i = 0
+
+    while i < len(L):
+        if L[i] == i + 1:
+            i += 1
+        else:
+            temp = L[L[i] - 1]
+            L[L[i] - 1] = L[i]
+            L[i] = temp
+    
+    return L
+
+
+    
+
+if __name__ == '__main__':
+    L = [5, 2, 6, 7, 1, 4, 3]
+    print("Sorted array: ", linear_sort(L))
